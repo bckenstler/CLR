e00eb58d50ea59b02871e7ce48c33ee6287a201c
Brad Kenstler <bckenstler@dons.usfca.edu>
2017-03-22 01:24:44 -0700

    Update clr_optimizers.py

--- clr_optimizers.py
+++ clr_optimizers.py
@@ -1,3 +1,58 @@
+"""
+Modified default Keras optimizer module https://github.com/fchollet/keras/blob/master/keras/optimizers.py
+
+Each optimizer can now implement cyclical learning rate with one of the following three approaches:
+
+"triangular":
+
+This method is a simple triangular cycle.
+
+To use triangular clr, simply pass into any optimizer the following:
+
+clr = {
+    "mode":"triangular",
+    "step_size":step_size,
+    "max_lr":max_lr
+    }
+    
+Where step_size is half the period of the cycle in iterations,
+and max_lr is the peak of the cycle (default lr is the base lr).
+
+"triangular2":
+
+This method is a triangular cycle that decreases the cycle amplitude by half after each period, while keeping the base lr constant.
+
+To use triangular2 clr, simply pass into any optimizer the following:
+
+clr = {
+    "mode":"triangular2",
+    "step_size":step_size,
+    "max_lr":max_lr
+    }
+    
+Where step_size is half the period of the cycle in iterations,
+and max_lr is the peak of the cycle (default lr is the base lr).
+
+"exp_range":
+
+This method is a triangular cycle that scales the cycle amplitude by a factor `gamma**(iterations)` at each iteration, while keeping the base lr constant.
+
+To use exp_range clr, simply pass into any optimizer the following:
+
+clr = {
+    "mode":"exp_range",
+    "step_size":step_size,
+    "max_lr":max_lr,
+    "gamma": gamma
+    }
+    
+Where step_size is half the period of the cycle in iterations,
+max_lr is the peak of the cycle (default lr is the base lr),
+and gamma is the scaling factor.
+
+Note that clr happens prior to any further learning rate adjustments as called for in a given optimizer, with the exception of learning rate decay. If implemented, decay happens prior to clr; this alters the cycle by lowering the minimum lr; the max_lr is kept constant.
+"""
+
 from __future__ import absolute_import
 import six
 from six.moves import zip
