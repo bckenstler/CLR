15bbad727c2ebc6eb8f219baeea18c9be0f75971
Jeremy Jordan <13970565+jeremyjordan@users.noreply.github.com>
2018-03-01 09:30:51 -0500

    fix on_batch_end to update lr after logging

--- clr_callback.py
+++ clr_callback.py
@@ -121,14 +121,11 @@ class CyclicLR(Callback):
         logs = logs or {}
         self.trn_iterations += 1
         self.clr_iterations += 1
-        K.set_value(self.model.optimizer.lr, self.clr())
 
         self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
         self.history.setdefault('iterations', []).append(self.trn_iterations)
 
         for k, v in logs.items():
             self.history.setdefault(k, []).append(v)
-            
-            
-
-  
+        
+        K.set_value(self.model.optimizer.lr, self.clr())
